b23c652af9c5ca0afced61b00b89515408271e14
Kevin Lee <kevinlee@wustl.edu>
2017-02-22 10:24:05 -0600

    Initial Commit

---
+++ Restaurant-Roulette/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Restaurant-Roulette
+//
+//  Created by Kevin Lee on 2/22/17.
+//  Copyright © 2017 Kevin Lee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
