0e10ef374c27626c95c31ad45a6358e141a0a8bd
Kevin Lee <kevinlee@wustl.edu>
2017-02-24 13:26:38 -0600

    wheel start

--- Restaurant-Roulette/ViewController.swift
+++ Restaurant-Roulette/ViewController.swift
@@ -9,7 +9,8 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    var position = 0;
+    @IBOutlet weak var wheel: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +21,13 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func spin(_ sender: Any) {
+        position = (position + 700)%360
+        UIView.animate(withDuration: 1.0, animations: {
+            self.wheel.transform = CGAffineTransform(rotationAngle: CGFloat(self.position))
+        })
+        
+    }
 
 }
 
