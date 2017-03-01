f60f99e1d71c1e513ed13cd71f738424482f8457
Kevin Lee <kevinlee@wustl.edu>
2017-03-01 17:13:57 -0600

    registers selected option

--- Restaurant-Roulette/ViewController.swift
+++ Restaurant-Roulette/ViewController.swift
@@ -9,21 +9,15 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var chosen: UITextField!
+    var options = ["1", "2", "3", "4"]
     var position = 0.0
     var hasFinishedSpinning = true
     @IBOutlet weak var wheel: UIImageView!
-    //var button = UIButton(frame: CGRect(x:100, y: 100, width: 70, height: 70))
     @IBOutlet weak var spinButton: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
-        //let button = UIButton(frame: CGRect(x:0, y: 0, width: 70, height: 70))
-        //button.center = self.view.center
         print(self.view.center.x.description + " " + self.view.center.y.description)
-        
-        //button.backgroundColor = .green
-        //button.setTitle("spinner", for: .normal)
-        //button.addTarget(self, action: #selector(spin2), for: .touchUpInside)
-        //self.view.addSubview(button)
         wheel.image = textToImage(drawText: "1", inImage: wheel.image!, atPoint: CGPoint(x: 195, y:75))
         wheel.image = textToImage(drawText: "3", inImage: wheel.image!, atPoint: CGPoint(x: 195, y: 320))
         wheel.image = textToImage(drawText: "4", inImage: wheel.image!, atPoint: CGPoint(x: 72.5, y: 197.5))
@@ -59,20 +53,20 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    //func spin2(sender: UIButton!){
-        //button.backgroundColor = .yellow
-    //    print("hello")
-   // }
     
     @IBAction func spin(_ sender: Any) {
         if (hasFinishedSpinning){
             hasFinishedSpinning = false
-            let spinResult = M_PI*4*(Double(arc4random())/0xFFFFFFFF) + M_PI*2
+            var spinResult = M_PI*4*(Double(arc4random())/0xFFFFFFFF) + M_PI*2
+            if ((spinResult - position) < M_PI*2){
+                spinResult += M_PI*2
+            }
             let spinAnimation = CABasicAnimation()
             spinAnimation.fromValue = position
             spinAnimation.toValue = spinResult
             spinAnimation.duration = 5
             spinAnimation.repeatCount = 0
+            spinAnimation.isAdditive = true
             spinAnimation.isRemovedOnCompletion = false
             spinAnimation.fillMode = kCAFillModeForwards
             spinAnimation.timingFunction = CAMediaTimingFunction(name: kCAMediaTimingFunctionEaseOut)
@@ -80,22 +74,26 @@ class ViewController: UIViewController {
             DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + 5) {
                 self.position = spinResult.truncatingRemainder(dividingBy: M_PI*2)
                 self.hasFinishedSpinning = true
-                print(self.position)
+                print(self.position/M_PI)
+                self.chosen.text = self.chooseBasedOnPosition(atPosition: self.position)
             }
             
         }
 
     }
-/*
-    @IBAction func spin(_ sender: Any) {
-        position = (position + 700)%360
-        UIView.animate(withDuration: 1.0, animations: {
-            self.wheel.transform = CGAffineTransform(rotationAngle: CGFloat(self.position))
-        })
-        
+    func chooseBasedOnPosition(atPosition pos: Double) -> String{
+        if (pos <= M_PI_4 || pos > 7*M_PI_4){
+            return options[0]
+        }
+        else if (pos <= 3*M_PI_4 && pos > M_PI_4) {
+            return options[3]
+        }
+        else if (pos <= 5*M_PI_4 && pos > 3*M_PI_4){
+            return options[2]
+        }
+        else {
+            return options[1]
+        }
     }
- */
-    
-
 }
 
