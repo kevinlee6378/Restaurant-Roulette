be4cba8194f3ba0de37c0e75b0e924680b439210
Kevin Lee <kevinlee@wustl.edu>
2017-03-03 13:58:15 -0600

    midterm update

--- Restaurant-Roulette/ViewController.swift
+++ Restaurant-Roulette/ViewController.swift
@@ -10,17 +10,54 @@ import UIKit
 
 class ViewController: UIViewController {
     var myCustomView: UIView!
+    var buttonArray: [UIButton] = []
     @IBOutlet weak var chosen: UITextField!
     var options = ["1", "2", "3", "4"]
     let rect1 = CGRect(x: 195, y: 75, width: 120, height: 100)
     var position = 0.0
     var option = ""
+    var optionIndex = 0
     var hasFinishedSpinning = true
     @IBOutlet weak var wheel: UIImageView!
     @IBOutlet weak var spinButton: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
+        wheel.isUserInteractionEnabled = true
+        
         chosen.textAlignment = .center
+        let shiftx = 90.0
+        let shifty = 110.0
+        let button1 = UIButton(frame: CGRect(x:195 - shiftx, y: 75 - shifty + 50, width:120, height:50))
+        button1.setTitle(options[0], for: .normal)
+        button1.backgroundColor = .black
+        button1.addTarget(self, action: #selector(showDetails), for: .touchUpInside)
+        buttonArray.append(button1)
+        let button4 = UIButton(frame: CGRect(x:317.5 - shiftx, y: 197.5 - shifty, width:60, height:100))
+        button4.setTitle(options[1], for: .normal)
+        
+        button4.backgroundColor = .blue
+        button4.addTarget(self, action: #selector(showDetails), for: .touchUpInside)
+        
+        buttonArray.append(button4)
+        let button2 = UIButton(frame: CGRect(x:195 - shiftx, y: 320 - shifty, width:120, height:50))
+        button2.backgroundColor = .yellow
+        button2.addTarget(self, action: #selector(showDetails), for: .touchUpInside)
+        
+        button2.setTitle(options[2], for: .normal)
+        
+        buttonArray.append(button2)
+        let button3 = UIButton(frame: CGRect(x:72.5 - shiftx + 60, y: 197.5 - shifty, width:60, height:100))
+        button3.backgroundColor = .red
+        button3.addTarget(self, action: #selector(showDetails), for: .touchUpInside)
+        button3.setTitle(options[3], for: .normal)
+        
+        buttonArray.append(button3)
+        
+        for button in buttonArray{
+            wheel.addSubview(button)
+        }
+        hideButtons()
+
         print(self.view.center.x.description + " " + self.view.center.y.description)
         wheel.image = textToImage(drawText: "1", inImage: wheel.image!, atPoint: CGPoint(x: 195, y:75))
         wheel.image = textToImage(drawText: "3", inImage: wheel.image!, atPoint: CGPoint(x: 195, y: 320))
@@ -29,14 +66,7 @@ class ViewController: UIViewController {
         wheel.center = self.view.center
         spinButton.center = self.view.center
         spinButton.layer.cornerRadius = 35
-        let button = UIButton(frame: rect1)
-        let x = self.view.center.x
-        let y = self.view.center.y - 90
-        button.center = CGPoint(x: x, y: y)
-        //button.backgroundColor = .black
-        button.addTarget(self, action: #selector(showDetails), for: .touchUpInside)
-
-        self.view.addSubview(button)
+        
     }
     
     func loadCustomViewIntoController()
@@ -108,6 +138,7 @@ class ViewController: UIViewController {
     
     @IBAction func spin(_ sender: Any) {
         if (hasFinishedSpinning){
+            hideButtons()
             hasFinishedSpinning = false
             var spinResult = M_PI*4*(Double(arc4random())/0xFFFFFFFF) + M_PI*2
             if ((spinResult - position) < M_PI*2){
@@ -129,10 +160,32 @@ class ViewController: UIViewController {
                 self.hasFinishedSpinning = true
                 print(self.position/M_PI)
                 self.chosen.text = self.option
+                self.optionIndex = self.chooseIndexBasedOnPosition(atPosition: self.position)
+                self.buttonArray[self.optionIndex].isHidden = false
+                print(self.optionIndex)
             }
             
         }
 
+    }
+    func hideButtons() {
+        for button in self.buttonArray{
+            button.isHidden = true
+        }
+    }
+    func chooseIndexBasedOnPosition(atPosition pos: Double) -> Int {
+        if (pos <= M_PI_4 || pos > 7*M_PI_4){
+            return 0
+        }
+        else if (pos <= 3*M_PI_4 && pos > M_PI_4) {
+            return 3
+        }
+        else if (pos <= 5*M_PI_4 && pos > 3*M_PI_4){
+            return 2
+        }
+        else {
+            return 1
+        }
     }
     func chooseBasedOnPosition(atPosition pos: Double) -> String{
         if (pos <= M_PI_4 || pos > 7*M_PI_4){
